ffeb148b6f305d6e7088a2f1c5df32e9596c67dc
Alex Whalen <147879389+Awhalen1999@users.noreply.github.com>
2025-10-30 16:45:53 -0230

    commit

--- components/header.tsx
+++ components/header.tsx
@@ -5,6 +5,7 @@ import { useRouter } from "next/navigation";
 export default function Header() {
   const router = useRouter();
 
+  //
   return (
     <header className="flex items-center justify-between px-8 py-4">
       <h1
